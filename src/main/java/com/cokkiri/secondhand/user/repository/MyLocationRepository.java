771cbfaa036c6f46cb1f2be43d883fa1fa27a444
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-08-30 12:16:49 +0900

    rename: jpa repository명 변경#2

--- src/main/java/com/cokkiri/secondhand/user/repository/MyLocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cokkiri.secondhand.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cokkiri.secondhand.user.entity.MyLocation;
-
-public interface MyLocationRepository extends JpaRepository<MyLocation, Long> {
-
-}
