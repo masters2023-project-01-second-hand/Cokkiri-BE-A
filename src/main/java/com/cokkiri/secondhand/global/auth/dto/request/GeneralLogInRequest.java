2984a301155db7370544af3f2767440544dd8060
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-02 10:59:42 +0900

    refactor: GeneralLogInRequest 미사용 코드 제거#10

--- src/main/java/com/cokkiri/secondhand/global/auth/dto/request/GeneralLogInRequest.java
+++ src/main/java/com/cokkiri/secondhand/global/auth/dto/request/GeneralLogInRequest.java
@@ -3,7 +3,6 @@ package com.cokkiri.secondhand.global.auth.dto.request;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -22,8 +21,4 @@ public class GeneralLogInRequest {
 	@NotBlank(message = "비밀번호는 필수 값입니다.")
 	@Length(max = 100, message = "비밀번호는 최대 100글자 입니다.")
 	private String password;
-
-	public String getEncodedPassword(PasswordEncoder passwordEncoder) {
-		return passwordEncoder.encode(this.password);
-	}
 }
