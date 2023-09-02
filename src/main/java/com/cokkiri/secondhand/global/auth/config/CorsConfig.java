cef47d49325e1b57ee00716a7ea41f2657a8ddcb
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-02 19:01:07 +0900

    feat: CORS 헤더 설정 추가#10

--- src/main/java/com/cokkiri/secondhand/global/auth/config/CorsConfig.java
+++ src/main/java/com/cokkiri/secondhand/global/auth/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.cokkiri.secondhand.global.auth.config;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +15,10 @@ public class CorsConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http//localhost:8080", "http//localhost:5173"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		configuration.setAllowedOrigins(List.of("http//localhost:8080", "http//localhost:5173"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setExposedHeaders(List.of("*"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
