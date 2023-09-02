f86e572c4504c38dac9c5f3b133ca182e3ccbe38
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-02 21:38:47 +0900

    refactor: 에러 메세지 상수화 및 누락된 에러코드 추가#10

--- src/main/java/com/cokkiri/secondhand/global/exception/list/NotExistAccessTokenException.java
+++ src/main/java/com/cokkiri/secondhand/global/exception/list/NotExistAccessTokenException.java
@@ -5,9 +5,11 @@ import com.cokkiri.secondhand.global.exception.CustomExceptionType;
 
 public class NotExistAccessTokenException extends CustomException {
 
+	private static final String ERROR_MESSAGE_FORMAT = "Access 토큰이 존재하지 않습니다.";
+
 	public NotExistAccessTokenException() {
 		super(
-			"Access 토큰이 존재하지 않습니다.",
+			ERROR_MESSAGE_FORMAT,
 			CustomExceptionType.NOT_EXIST_ACCESS_TOKEN_EXCEPTION);
 	}
 }
