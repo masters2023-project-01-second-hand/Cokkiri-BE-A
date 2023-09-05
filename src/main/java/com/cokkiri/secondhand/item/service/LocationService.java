7a5cd5666e47b64c775e6daf2568477957d83203
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-05 14:49:12 +0900

    feat: 전체 지역 검색 api 한 글자도 검색 가능하도록 개선#12

--- src/main/java/com/cokkiri/secondhand/item/service/LocationService.java
+++ src/main/java/com/cokkiri/secondhand/item/service/LocationService.java
@@ -1,9 +1,10 @@
 package com.cokkiri.secondhand.item.service;
 
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,19 +18,60 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class LocationService {
 
+	private static final String DEFAULT_KEYWORD = "서울 역삼";
+	private static final String SINGLE_LETTER_REGEX = "[ㄱ-ㅎㅏ-ㅣ]";
+
+	private final Pattern pattern = Pattern.compile(SINGLE_LETTER_REGEX);
+
 	private final LocationSearchJpaRepository locationSearchJpaRepository;
 
 	@Transactional
-	public LocationNameListResponse searchLocations(String keyword, int cursor) {
+	public LocationNameListResponse searchLocations(String keyword, Pageable pageable) {
+		keyword = removeSingleLetter(keyword);
 
-		Pageable pageable = PageRequest.of(cursor, 10);
+		switch (countNumberOfCharactersBy(keyword)) {
+			case 0:
+				return searchLocationsFor2OrMoreCharacter(DEFAULT_KEYWORD, pageable);
+			case 1:
+				return searchLocationsForOneCharacter(keyword, pageable);
+			default:
+				return searchLocationsFor2OrMoreCharacter(keyword, pageable);
+		}
+	}
 
-		List<LocationNameResponse> locationNames = locationSearchJpaRepository.search(keyword, pageable)
-			.map(LocationNameResponse::from).getContent();
+	private LocationNameListResponse searchLocationsForOneCharacter(String keyword, Pageable pageable) {
+		Slice<LocationNameResponse> locationNames = locationSearchJpaRepository.searchForOneCharacter(keyword, pageable)
+			.map(LocationNameResponse::from);
 
 		return new LocationNameListResponse(
-			locationNames,
-			cursor + 1L
+			locationNames.getContent(),
+			calculateNextPage(locationNames)
 		);
+	}
+
+	private LocationNameListResponse searchLocationsFor2OrMoreCharacter(String keyword, Pageable pageable) {
+		Slice<LocationNameResponse> locationNames = locationSearchJpaRepository.searchFor2OrMoreCharacter(keyword, pageable)
+			.map(LocationNameResponse::from);
+
+		return new LocationNameListResponse(
+			locationNames.getContent(),
+			calculateNextPage(locationNames)
+		);
+	}
+
+	private String removeSingleLetter(String keyword) {
+		Matcher matcher = pattern.matcher(keyword);
+		return matcher.replaceAll("");
+	}
+
+	private int countNumberOfCharactersBy(String keyword) {
+		return keyword.replace(" ", "").length();
+	}
+
+	private Long calculateNextPage(Slice<LocationNameResponse> locationNames) {
+		if(locationNames.hasNext()) {
+			return locationNames.getNumber() + 2L;
+		}
+		return null;
 	}
 }
