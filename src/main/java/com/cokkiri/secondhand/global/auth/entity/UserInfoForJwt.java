14060b6aa6e17e4c296c9c1949443c1b8416e4f8
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-05 14:44:41 +0900

    feat: 내 지역 목록 불러오기 api 추가#12

--- src/main/java/com/cokkiri/secondhand/global/auth/entity/UserInfoForJwt.java
+++ src/main/java/com/cokkiri/secondhand/global/auth/entity/UserInfoForJwt.java
@@ -55,6 +55,10 @@ public class UserInfoForJwt {
 		return Map.of("id", String.valueOf(this.userId), "userType", userType.name());
 	}
 
+	public Long getIdAsLong() {
+		return Long.valueOf(this.id);
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
