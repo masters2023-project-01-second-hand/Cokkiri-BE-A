3f522a7a8fea90806adba329368264a9f154f43f
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-12 13:03:01 +0900

    feat: 카테고리 추천 목록 불러오기 api 추가#24

--- src/main/java/com/cokkiri/secondhand/item/service/CategoryService.java
+++ src/main/java/com/cokkiri/secondhand/item/service/CategoryService.java
@@ -1,11 +1,14 @@
 package com.cokkiri.secondhand.item.service;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.cokkiri.secondhand.item.dto.response.CategoryListResponse;
 import com.cokkiri.secondhand.item.dto.response.CategoryResponse;
+import com.cokkiri.secondhand.item.entity.Category;
 import com.cokkiri.secondhand.item.repository.CategoryJpaRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,21 @@ public class CategoryService {
 		return
 			new CategoryListResponse(
 				categoryJpaRepository.findAll()
+					.stream()
+					.map(CategoryResponse::from)
+					.collect(Collectors.toList())
+			);
+	}
+
+	public CategoryListResponse getRecommendedCategories() {
+		List<Category> categories = categoryJpaRepository.findAll();
+
+		Collections.shuffle(categories);
+		List<Category> recommendedCategories = categories.subList(0, 3);
+
+		return
+			new CategoryListResponse(
+				recommendedCategories
 					.stream()
 					.map(CategoryResponse::from)
 					.collect(Collectors.toList())
