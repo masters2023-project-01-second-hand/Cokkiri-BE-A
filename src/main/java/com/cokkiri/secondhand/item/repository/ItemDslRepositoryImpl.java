188d10eb12267e5d4d749d8dbb8c51ef6265e29b
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-12 11:59:14 +0900

    refactor: Item 목록 불러오기 QueryDSL 로직 개선#22

--- src/main/java/com/cokkiri/secondhand/item/repository/ItemDslRepositoryImpl.java
+++ src/main/java/com/cokkiri/secondhand/item/repository/ItemDslRepositoryImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import com.cokkiri.secondhand.item.entity.Item;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Repository
@@ -35,7 +36,9 @@ public class ItemDslRepositoryImpl implements ItemDslRepository {
 		return queryFactory
 			.selectFrom(item)
 			.leftJoin(location).on(location.id.eq(item.location.id))
-			.where(dynamicLtId.and(location.id.eq(locationId)))
+			.where(
+				cursorCondition(cursorId),
+				location.id.eq(locationId))
 			.orderBy(item.id.desc())
 			.limit(pageable.getPageSize())
 			.fetch();
@@ -43,22 +46,20 @@ public class ItemDslRepositoryImpl implements ItemDslRepository {
 
 	public List<Item> findAllByCategoryIdAndLocationId(Pageable pageable, Long categoryId, Long locationId, Long cursorId) {
 
-		BooleanBuilder dynamicLtId = new BooleanBuilder();
-
-		if (cursorId != null) {
-			dynamicLtId.and(item.id.lt(cursorId));
-		}
-
 		return queryFactory
 			.selectFrom(item)
 			.leftJoin(category).on(category.id.eq(item.category.id))
 			.leftJoin(location).on(location.id.eq(item.location.id))
 			.where(
-				dynamicLtId
-					.and(category.id.eq(categoryId))
-					.and(location.id.eq(locationId)))
+					cursorCondition(cursorId)
+					, category.id.eq(categoryId)
+					, location.id.eq(locationId))
 			.orderBy(item.id.desc())
 			.limit(pageable.getPageSize())
 			.fetch();
+	}
+
+	private BooleanExpression cursorCondition(Long cursorId) {
+		return cursorId != null? item.id.lt(cursorId) : null;
 	}
 }
