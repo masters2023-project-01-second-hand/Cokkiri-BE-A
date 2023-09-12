6b938fecf228e9a31ca3fcd1e01350ed34556490
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-12 11:27:42 +0900

    refactor: UserType.getUserTypeBy() 로직 개선#22

--- src/main/java/com/cokkiri/secondhand/user/entity/UserType.java
+++ src/main/java/com/cokkiri/secondhand/user/entity/UserType.java
@@ -1,14 +1,15 @@
 package com.cokkiri.secondhand.user.entity;
 
+import java.util.Arrays;
+
 public enum UserType {
 	GUEST, GENERAL, GITHUB;
 
 	public static UserType getUserTypeBy(String name) {
-		for(UserType type : UserType.values()) {
-			if (type.name().equals(name)) {
-				return type;
-			}
-		}
-		return UserType.GUEST;
+
+		return Arrays.stream(UserType.values())
+			.filter(type -> type.name().equals(name))
+			.findFirst()
+			.orElse(UserType.GUEST);
 	}
 }
