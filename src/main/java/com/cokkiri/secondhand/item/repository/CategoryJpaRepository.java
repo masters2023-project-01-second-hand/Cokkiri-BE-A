a36aa9805434bb2b4cffae5e5da79ae8bde0a76e
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-12 12:27:05 +0900

    refactor: JPA Repository 필요없는 메서드 시그니처 제거#22

--- src/main/java/com/cokkiri/secondhand/item/repository/CategoryJpaRepository.java
+++ src/main/java/com/cokkiri/secondhand/item/repository/CategoryJpaRepository.java
@@ -1,12 +1,8 @@
 package com.cokkiri.secondhand.item.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cokkiri.secondhand.item.entity.Category;
 
 public interface CategoryJpaRepository extends JpaRepository<Category, Long> {
-
-	List<Category> findAll();
 }
