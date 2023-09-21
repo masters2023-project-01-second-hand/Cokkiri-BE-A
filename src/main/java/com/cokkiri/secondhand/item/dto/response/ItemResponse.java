55d240631f78684b9db66394fab2b6b0b3da9778
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-09-21 18:22:15 +0900

    fix: 아이템 목록 불러오기 시 섬네일 이미지도 불러오도록 변경#30

--- src/main/java/com/cokkiri/secondhand/item/dto/response/ItemResponse.java
+++ src/main/java/com/cokkiri/secondhand/item/dto/response/ItemResponse.java
@@ -1,10 +1,13 @@
 package com.cokkiri.secondhand.item.dto.response;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import com.cokkiri.secondhand.global.auth.entity.UserInfoForJwt;
 import com.cokkiri.secondhand.item.entity.Item;
+import com.cokkiri.secondhand.item.entity.ItemImage;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,6 +18,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class ItemResponse {
+
+	// 아이템 이미지 리스트의 첫 번째 이미지를 섬네일 이미지로 선정한다.
+	private static final int THUMBNAIL_IMAGE_INDEX = 0;
 
 	private Long id;
 	private String title;
@@ -35,8 +41,16 @@ public class ItemResponse {
 			item.getStatus().getStatusName(),
 			item.getPrice(),
 			null, //CountDataResponse.from(item.getItemMetadata()),	// TODO: countData 추가하기
-			null,			// TODO: image 추가하기
+			extractThumbnailUrl(item),			// TODO: image 추가하기
 			isSeller(item, userInfoForJwt));			// TODO: isSeller 추가하기
+	}
+
+	private static String extractThumbnailUrl(Item item) {
+		List<ItemImage> itemImage =new ArrayList<>();
+		if(item.getItemImages().isEmpty()) {
+			return null;
+		}
+		return item.getItemImages().get(THUMBNAIL_IMAGE_INDEX).getUrl();
 	}
 
 	private static boolean isSeller(Item item, UserInfoForJwt userInfoForJwt) {
