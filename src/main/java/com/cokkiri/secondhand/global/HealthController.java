38a72568c9b9ebb2f287361685fc7aa11ac8e34d
dokkisan <dokkisan.wang@gmail.com>
2023-09-23 19:42:54 +0900

    chore: HealthController 수정#41

--- src/main/java/com/cokkiri/secondhand/global/HealthController.java
+++ src/main/java/com/cokkiri/secondhand/global/HealthController.java
@@ -3,10 +3,14 @@ package com.cokkiri.secondhand.global;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HealthController {
-	@GetMapping("/health")
-	public String healthCheck() {
+	@GetMapping("/health_was")
+	public String healthCheckWas() {
+		log.info("Health!");
 		return "Health!";
 	}
 }
