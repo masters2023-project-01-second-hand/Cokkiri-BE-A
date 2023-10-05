e19ab6c4278e7596a34723b10181ca23f7b57e63
dokkisan <dokkisan.wang@gmail.com>
2023-10-05 20:30:22 +0900

    chore: 모든 유입 허용하도록 임시 수정 #57

--- src/main/java/com/cokkiri/secondhand/global/auth/config/CorsConfig.java
+++ src/main/java/com/cokkiri/secondhand/global/auth/config/CorsConfig.java
@@ -2,7 +2,7 @@ package com.cokkiri.secondhand.global.auth.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
