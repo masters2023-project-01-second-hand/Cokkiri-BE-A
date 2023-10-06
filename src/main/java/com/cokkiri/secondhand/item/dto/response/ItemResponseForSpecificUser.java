b2b4945834d54efa488d12763702b59ffdaae5f4
dokkisan <dokkisan.wang@gmail.com>
2023-10-06 15:49:36 +0900

    fix: isSeller 누락 수정을 위한  userInfoForJwt 추가#57

--- src/main/java/com/cokkiri/secondhand/item/dto/response/ItemResponseForSpecificUser.java
+++ src/main/java/com/cokkiri/secondhand/item/dto/response/ItemResponseForSpecificUser.java
@@ -1,5 +1,6 @@
 package com.cokkiri.secondhand.item.dto.response;
 
+import com.cokkiri.secondhand.global.auth.entity.UserInfoForJwt;
 import com.cokkiri.secondhand.item.entity.Item;
 
 import lombok.AccessLevel;
@@ -10,11 +11,11 @@ import lombok.Getter;
 @Getter
 public class ItemResponseForSpecificUser extends ItemResponse {
 
-	public static ItemResponseForSpecificUser from(Item item) {
-		return new ItemResponseForSpecificUser(item);
+	public static ItemResponseForSpecificUser from(Item item, UserInfoForJwt userInfoForJwt) {
+		return new ItemResponseForSpecificUser(item, userInfoForJwt);
 	}
 
-	private ItemResponseForSpecificUser(Item item) {
-		super(item);
+	private ItemResponseForSpecificUser(Item item, UserInfoForJwt userInfoForJwt) {
+		super(item, userInfoForJwt);
 	}
 }
